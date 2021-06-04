926c1bc2e5d6a8581046f857c1e24513fb35816e
Spotlight <spotlight@joscomputing.space>
2021-06-04 02:03:01 -0500

    Enforce a coding style

--- source/main.c
+++ source/main.c
@@ -7,31 +7,31 @@ static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
 int main(int argc, char **argv) {
-  VIDEO_Init();
-  WPAD_Init();
-  rmode = VIDEO_GetPreferredMode(NULL);
-  xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-  console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight,
-               rmode->fbWidth * VI_DISPLAY_PIX_SZ);
-  VIDEO_Configure(rmode);
-  VIDEO_SetNextFramebuffer(xfb);
-  VIDEO_SetBlack(FALSE);
-  VIDEO_Flush();
-  VIDEO_WaitVSync();
-  if (rmode->viTVMode & VI_NON_INTERLACE)
+    VIDEO_Init();
+    WPAD_Init();
+    rmode = VIDEO_GetPreferredMode(NULL);
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    console_init(xfb, 20, 20, rmode->fbWidth, rmode->xfbHeight,
+                 rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(xfb);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
     VIDEO_WaitVSync();
+    if (rmode->viTVMode & VI_NON_INTERLACE)
+        VIDEO_WaitVSync();
 
-  printf("Open the shop, we said.\n");
+    printf("Open the shop, we said.\n");
 
-  while (1) {
-    WPAD_ScanPads();
-    u32 pressed = WPAD_ButtonsDown(0);
+    while (1) {
+        WPAD_ScanPads();
+        u32 pressed = WPAD_ButtonsDown(0);
 
-    if (pressed & WPAD_BUTTON_HOME)
-      exit(0);
+        if (pressed & WPAD_BUTTON_HOME)
+            exit(0);
 
-    VIDEO_WaitVSync();
-  }
+        VIDEO_WaitVSync();
+    }
 
-  return 0;
+    return 0;
 }
