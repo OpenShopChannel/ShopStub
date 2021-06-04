77c0c1cbb76f06ee99564040bf850f02f1e1a608
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-04 10:29:51 -0400

    Add USB detection

--- source/main.c
+++ source/main.c
@@ -11,12 +11,48 @@ static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
 const DISC_INTERFACE *sd_slot = &__io_wiisd;
-// TODO: Handle USB drive access
-// const DISC_INTERFACE* usb = &__io_usbstorage;
+const DISC_INTERFACE* usb = &__io_usbstorage;
 
-void SDCard_Init() {
+void returnToMenu() {
+    while (1) {
+        WPAD_ScanPads();
+        u32 pressed = WPAD_ButtonsDown(0);
+
+        if (pressed & WPAD_BUTTON_HOME)
+            exit(0);
+
+        VIDEO_WaitVSync();
+    }
+}
+
+void Init_IO() {
+    // Initialize IO
+    usb->startup();
     sd_slot->startup();
-    fatMountSimple("sd", &__io_wiisd);
+
+    // Check if the SD Card is inserted
+    bool isInserted = __io_wiisd.isInserted();
+
+    // Try to mount the SD Card before the USB
+    if (isInserted) {
+        fatMountSimple("fat", sd_slot);
+    } else {
+        // Since the SD Card is not inserted, we will attempt to mount the USB.
+        bool idk = __io_usbstorage.isInserted();
+        if (idk) {
+            fatMountSimple("fat", usb);
+        } else {
+            // No input devices were inserted OR it failed to mount either device.
+            printf("Please insert either an SD Card or USB.\n");
+            returnToMenu();
+        }
+    }
+}
+
+void DeInit_IO() {
+    fatUnmount("fat:/");
+    __io_usbstorage.shutdown();
+    __io_wiisd.shutdown();
 }
 
 int main(int argc, char **argv) {
@@ -38,24 +74,19 @@ int main(int argc, char **argv) {
 
     ISFS_Initialize();
     CONF_Init();
-    SDCard_Init();
-    s32 unzip = unzipFile("sd:/osc-temp/wiilauncher.zip",
-                          "apps/wiilauncher/boot.dol", "sd:/moment.dol");
+    Init_IO();
+
+    s32 unzip = unzipFile("fat:/osc-temp/wiilauncher.zip",
+                          "apps/wiilauncher/boot.dol", "fat:/moment.dol");
     if (unzip > 0) {
         printf("Epic fail");
     } else {
         printf("success");
     }
 
-    while (1) {
-        WPAD_ScanPads();
-        u32 pressed = WPAD_ButtonsDown(0);
+    DeInit_IO();
 
-        if (pressed & WPAD_BUTTON_HOME)
-            exit(0);
-
-        VIDEO_WaitVSync();
-    }
+    returnToMenu();
 
     return 0;
 }
